fdbfe271363831fa524a0ee088fd7b26d8a0ec40
Kirill-Elistratov <elistratovkirill3@gmail.com>
2024-05-10 19:23:30 +0500

    добавляем обработку нажатий

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -1,2 +1,25 @@
 import kb
 from handlers.client import *
+
+
+async def handle_game_actions(callback_query: types.CallbackQuery):
+    action = callback_query.data
+    if action == 'clash_royale_action':
+        await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)
+        await bot.send_message(callback_query.from_user.id, f'👇Выберите товар👇', reply_markup=kb.clashroyale_products)
+
+    elif action == 'super_sus_action':
+        await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)
+        await bot.send_message(callback_query.from_user.id, '👇Выберите товар👇')
+
+    elif action == 'pubg_mobile_action':
+        await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)
+        await bot.send_message(callback_query.from_user.id, '👇Выберите товар👇')
+
+    elif action == 'clash_of_clans_action':
+        await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)
+        await bot.send_message(callback_query.from_user.id, '👇Выберите товар👇')
+
+    elif action == 'brawl_stars_action':
+        await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)
+        await bot.send_message(callback_query.from_user.id, '👇Выберите товар👇')
