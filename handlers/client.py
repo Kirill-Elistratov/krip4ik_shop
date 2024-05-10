901b9702c10f605d1ae0f57f5bb4b5c517d5fd02
Kirill-Elistratov <elistratovkirill3@gmail.com>
2024-05-10 19:00:47 +0500

    начинаем делать клиентскую часть бота - то, что будет видеть пользователь

---
+++ handlers/client.py
@@ -0,0 +1,19 @@
+import kb
+from configurebot import cfg
+from bans import banned_users
+
+welcomemessage = cfg['welcome_message']
+errormessage = cfg['error_message']
+devid = cfg['dev_id']
+aboutus = cfg['about_us']
+question_first_msg = cfg['question_type_ur_question_message']
+x = cfg['x']
+
+handler_button_new_question = cfg['button_new_question']
+handler_button_about_us = cfg['button_about_us']
+handler_button_rules = cfg['button_rules']
+handler_button_catalog = cfg['button_catalog']
+
+channel_username = cfg['tgk_id']
+NOTSUBMSG = cfg['not_sub_message']
+ban_message = cfg['ban_message']
