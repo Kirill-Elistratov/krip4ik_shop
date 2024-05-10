5cfd80fa8d5bb5b55b5095ae0c07024a772052a9
Kirill-Elistratov <elistratovkirill3@gmail.com>
2024-05-10 18:52:13 +0500

    добавляем кнопку отзывов и проверку на подписку на канал

--- kb.py
+++ kb.py
@@ -8,3 +8,14 @@ mainmenurules = KeyboardButton(cfg['button_rules'])
 mainmenucatalog = KeyboardButton(cfg['button_catalog'])
 mainmenu = ReplyKeyboardMarkup(resize_keyboard=True).row(mainmenunewsupport, mainmenuabout)
 mainmenu.row(mainmenurules, mainmenucatalog)
+
+btnreviews = InlineKeyboardButton(text='ОТЗЫВЫ', url='https://t.me/KRIP4IK1/12/13')
+reviewsmenu = InlineKeyboardMarkup(row_width=1)
+reviewsmenu.insert(btnreviews)
+
+btnUrlChannel = InlineKeyboardButton(text='ПОДПИСАТЬСЯ', url='https://t.me/kripchik5')
+btnDoneSub = InlineKeyboardButton(text='ПОДПИСАЛСЯ', callback_data='subchanneldone')
+
+checkSubMenu = InlineKeyboardMarkup(row_width=1)
+checkSubMenu.insert(btnUrlChannel)
+checkSubMenu.insert(btnDoneSub)
