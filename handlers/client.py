acedf644ac4ddc89de0f60d50ab0073e37186a6c
Kirill-Elistratov <elistratovkirill3@gmail.com>
2024-05-10 19:03:11 +0500

    добавляем обработку подписки на канал и команду старт

--- handlers/client.py
+++ handlers/client.py
@@ -17,3 +17,30 @@ handler_button_catalog = cfg['button_catalog']
 channel_username = cfg['tgk_id']
 NOTSUBMSG = cfg['not_sub_message']
 ban_message = cfg['ban_message']
+
+
+def check_sub_channel(chat_member):
+    print(chat_member['status'])
+    if chat_member['status'] == 'left':
+        return False
+    else:
+        return True
+
+
+async def client_start(message: types.Message):
+    try:
+        if message.chat.type != 'private':
+            await message.answer('Данную команду можно использовать только в личных сообщениях с ботом.')
+        else:
+            if check_sub_channel(await bot.get_chat_member(chat_id=channel_username, user_id=message.from_user.id)):
+                user_id = message.from_user.id
+                if user_id in banned_users and banned_users[user_id]:
+                    await message.answer(f'{ban_message}', parse_mode="Markdown")
+                else:
+                    await message.answer(f'{welcomemessage}', reply_markup=kb.mainmenu)
+            else:
+                await message.answer(f'{NOTSUBMSG}', reply_markup=kb.checkSubMenu)
+    except Exception as e:
+        cid = message.chat.id
+        await message.answer(f"{errormessage}", parse_mode='Markdown')
+        await bot.send_message(devid, f"Случилась *ошибка* в чате *{cid}*\nСтатус ошибки: {e}", parse_mode='Markdown')
